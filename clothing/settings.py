215b15d8dc045222fa11ead19645ada79a88e1bf
ericdev-202 <ericmuchiri764@gmail.com>
2021-11-14 17:51:50 +0300

    the

--- clothing/settings.py
+++ clothing/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 # import heroku_django
-# import django_heroku
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,4 +127,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
